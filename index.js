1fa5f9c67aa16e3cdb2d5f8c1f5acae8a6b4002f
Madhun13 <nainmadhu1316@gmail.com>
2025-08-12 16:49:30 +0530

    first commit StudyNotion

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-// const  hello=()=>{
-//    console.log("hello");
-// }
 
-// const function mohit  =(name , hello)=>{
-// console.log({$name} , hello);
-// }
-
-
-// hello this is mohit khatri 
 
 const hello = () => {
     console.log("hello");
  }
  
- const mohit = (name, hello) => {
+ const nain = (name, hello) => {
     console.log(`${name}`, hello);
  }
  
- mohit("Mohit", hello);
+ nain("mn", hello);
  
 
